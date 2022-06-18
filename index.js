4bb96dd2d480635a8d87bcc80f9f89cb3fc13f9c
Levi Barsis <lbarsis@gmail.com>
2022-06-18 11:00:07 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const newHeader = document.createElement("h1");
+const removeMain = document.getElementById("main")
+removeMain.remove("main")
+newHeader.id = 'victory';
+newHeader.textContent = 'Levi is the champion';
+document.body.append(newHeader);
